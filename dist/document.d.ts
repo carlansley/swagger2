461e237f5ca4107351c1ec6e5f1401a2ac31fdac
Carl Ansley <carl.ansley@gmail.com>
2022-08-18 13:43:49 -0400

    Add publish workflow, remove `dist`

--- dist/document.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Document } from './schema';
-export declare function validateDocument(document: unknown): Document | undefined;
-export declare function loadDocumentSync(file: string): unknown;
--- dist/schema.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// schema.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=schema.js.map
